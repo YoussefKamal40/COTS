037bd359e5ca91051acf424de56d3e613c39f6f8
YoussefKamal40 <youx199@gmail.com>
2020-07-26 03:14:03 +0200

    config example
    adding a configuration example  for LIN

--- HAL/LIN/LIN_config.c
+++ HAL/LIN/LIN_config.c
@@ -2,146 +2,286 @@
 #include "USART_interface.h"
 #include "LIN.h"
 
-static u8 signal4[SIGNAL4_LENGTH]={0,1,2};
-static u8 signal5[SIGNAL5_LENGTH]={3,4};
-static u8 signal6[SIGNAL6_LENGTH]={5};
-static u8 signal7[SIGNAL7_LENGTH]={6,7,8,9,10,11,12};
+/*
+	Mo ID4 uncond 1 byte bi2->sL0 bi3~bi4->sL1
+	Mi ID30 uncond 4 byte bi0~15->mL2 bi16~32->mL3
+	Mo ID23 sporadic 4 byte by0~2->mSw0sL4 by3->mSw1sL5
+	Mi ID40 eventTrig 5 byte by0->sSw2mL6 by1~4->sSw3mL7
+*/
 
-u16 scalerSignals[NUMBER_OF_SCALER_SIGNALS]={1,2,3,4};
-u8* const byteArraySignals[NUMBER_OF_BYTE_ARRAY_SIGNALS]={signal4,signal5,signal6,signal7};
+/*
+ * THeader_Nominal= 34 * TBit
+ * TResponse_Nominal= 10 * (NData + 1) * TBit
+ * TFrame_Nominal= THeader_Nominal + TResponse_Nominal
+ * TFrame_Nominal= (34+10*(NData+1))*TBit
+ * TFrame_Maximum=1.4*((34+10*(NData+1))*TBit)
+ * TFrame_Maximum=1.4*((34+10*(NData+1))/BIT_RATE)
+ * TFrame_Slot > jitter + TFrame_Maximum
+ * */
 
-static const LIN_signalConfigType frame_0_0_signals[NUMBER_OF_FRAME_0_0_SIGNALS]=
+static u8 mSw0Signal[3];
+static u8 mSw1Signal[1];
+static u8 mSw2Signal[1];
+static u8 mSw3Signal[4];
+static u8 sSw0Signal[3];
+static u8 sSw1Signal[1];
+static u8 sSw2Signal[1];
+static u8 sSw3Signal[4];
+
+u16 scalerSignals[NUMBER_OF_SCALER_SIGNALS]=
+{
+		mSw0Signal,
+		mSw1Signal,
+		mSw2Signal,
+		mSw3Signal,
+		sSw0Signal,
+		sSw1Signal,
+		sSw2Signal,
+		sSw3Signal
+};
+
+u8* const byteArraySignals[NUMBER_OF_BYTE_ARRAY_SIGNALS]={};
+
+static const LIN_signalConfigType mSw2Sw3Signals[2]=
 {
 		{
-				.ID=		SIGNAL0_ID,
-				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.ID=		SIGNAL_SW2_M,
+				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
 				.length=	1,
-				.offset=	24
-		},
-		{
-				.ID=		SIGNAL4_ID,
-				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
-				.length=	SIGNAL4_LENGTH,
-				.offset=	0
-		}
-};
-
-static const LIN_signalConfigType frame_0_1_signals[NUMBER_OF_FRAME_0_1_SIGNALS]=
-{
-		{
-				.ID=		SIGNAL1_ID,
-				.type=		LIN_SIGNAL_TYPE_SCALER,
-				.length=	2,
-				.offset=	16
-		},
-		{
-				.ID=		SIGNAL5_ID,
-				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
-				.length=	SIGNAL5_LENGTH,
-				.offset=	0
-		}
-};
-
-static const LIN_signalConfigType frame_1_0_signals[NUMBER_OF_FRAME_1_0_SIGNALS]=
-{
-		{
-				.ID=		SIGNAL2_ID,
-				.type=		LIN_SIGNAL_TYPE_SCALER,
-				.length=	2,
-				.offset=	8
-		},
-		{
-				.ID=		SIGNAL6_ID,
-				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
-				.length=	SIGNAL6_LENGTH,
-				.offset=	0
-		}
-};
-
-static const LIN_signalConfigType frame_1_1_signals[NUMBER_OF_FRAME_1_1_SIGNALS]=
-{
-		{
-				.ID=		SIGNAL3_ID,
-				.type=		LIN_SIGNAL_TYPE_SCALER,
-				.length=	3,
 				.offset=	0
 		},
 		{
-				.ID=		SIGNAL7_ID,
+				.ID=		SIGNAL_SW3_M,
 				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
-				.length=	SIGNAL7_LENGTH,
+				.length=	4,
 				.offset=	1
 		}
 };
 
-static const LIN_frameConfigType cluster0Frames[NUMBER_OF_CLUSTER_0_FRAMES]=
+static const LIN_signalConfigType sSw2Sw3Signals[2]=
 {
 		{
-				.signals=				frame_0_0_signals,
-				.periodicity=			0,
-				.ID=					0,
-				.type=					LIN_FRAME_TYPE_UNCONDITIONAL,
-				.numberOfSignals=		NUMBER_OF_FRAME_0_0_SIGNALS,
-				.frameSlotTimeTicks=	FRAME_0_0_TIME_SLOT,
-				.frameSize=				FRAME_0_0_SIZE,
-				.direction=				LIN_FRAME_DIRECTION_PUBLISHER,
-				.firstDelay=			0
+				.ID=		SIGNAL_SW2_S,
+				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
+				.length=	1,
+				.offset=	0
 		},
 		{
-				.signals=				frame_0_1_signals,
-				.periodicity=			0,
-				.ID=					1,
-				.type=					LIN_FRAME_TYPE_EVENT_TRIG,
-				.numberOfSignals=		NUMBER_OF_FRAME_0_1_SIGNALS,
-				.frameSlotTimeTicks=	FRAME_0_1_TIME_SLOT,
-				.frameSize=				FRAME_0_1_SIZE,
-				.direction=				LIN_FRAME_DIRECTION_PUBLISHER,
-				.firstDelay=			0
+				.ID=		SIGNAL_SW3_S,
+				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
+				.length=	4,
+				.offset=	1
 		}
 };
 
-static const LIN_frameConfigType cluster1Frames[NUMBER_OF_CLUSTER_1_FRAMES]=
+static const LIN_signalConfigType mSw0Sw1Signals[2]=
 {
 		{
-				.signals=				frame_1_0_signals,
-				.periodicity=			0,
-				.ID=					2,
+				.ID=		SIGNAL_SW0_M,
+				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
+				.length=	3,
+				.offset=	0
+		},
+		{
+				.ID=		SIGNAL_SW1_M,
+				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
+				.length=	1,
+				.offset=	3
+		}
+};
+
+static const LIN_signalConfigType sSw0Sw1Signals[2]=
+{
+		{
+				.ID=		SIGNAL_SW0_S,
+				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
+				.length=	3,
+				.offset=	0
+		},
+		{
+				.ID=		SIGNAL_SW1_S,
+				.type=		LIN_SIGNAL_TYPE_BYTE_ARRAY,
+				.length=	1,
+				.offset=	3
+		}
+};
+
+static const LIN_signalConfigType sL2L3Signals[2]=
+{
+		{
+				.ID=		SIGNAL_LED2_S,
+				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.length=	16,
+				.offset=	0
+		},
+		{
+				.ID=		SIGNAL_LED3_S,
+				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.length=	16,
+				.offset=	16
+		}
+};
+
+static const LIN_signalConfigType mL2L3Signals[2]=
+{
+		{
+				.ID=		SIGNAL_LED2_M,
+				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.length=	16,
+				.offset=	0
+		},
+		{
+				.ID=		SIGNAL_LED3_M,
+				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.length=	16,
+				.offset=	16
+		}
+};
+
+static const LIN_signalConfigType mL0L1Signals[2]=
+{
+		{
+				.ID=		SIGNAL_LED0_M,
+				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.length=	1,
+				.offset=	2
+		},
+		{
+				.ID=		SIGNAL_LED1_M,
+				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.length=	2,
+				.offset=	3
+		}
+};
+
+static const LIN_signalConfigType sL0L1Signals[2]=
+{
+		{
+				.ID=		SIGNAL_LED0_S,
+				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.length=	1,
+				.offset=	2
+		},
+		{
+				.ID=		SIGNAL_LED1_S,
+				.type=		LIN_SIGNAL_TYPE_SCALER,
+				.length=	2,
+				.offset=	3
+		}
+};
+
+static const LIN_frameConfigType cluster0Frames[4]=
+{
+		{/*output frame for led0 and led1 ID=4*/
+				.signals=				mL0L1Signals,
+				.periodicity=			10,
+				.ID=					4,
 				.type=					LIN_FRAME_TYPE_UNCONDITIONAL,
-				.numberOfSignals=		NUMBER_OF_FRAME_1_0_SIGNALS,
-				.frameSlotTimeTicks=	FRAME_1_0_TIME_SLOT,
-				.frameSize=				FRAME_1_0_SIZE,
+				.numberOfSignals=		2,
+				.frameSlotTimeTicks=	1,/*3.9375 + jitter=5ms*/
+				.frameSize=				1,
+				.direction=				LIN_FRAME_DIRECTION_PUBLISHER,
+				.firstDelay=			0
+		},
+		{/*input frame for led2 and led3 ID=30*/
+				.signals=				mL2L3Signals,
+				.periodicity=			25,
+				.ID=					30,
+				.type=					LIN_FRAME_TYPE_UNCONDITIONAL,
+				.numberOfSignals=		2,
+				.frameSlotTimeTicks=	1,/*6.125 + jitter = 7ms*/
+				.frameSize=				4,
+				.direction=				LIN_FRAME_DIRECTION_SUBSCRIBER,
+				.firstDelay=			2
+		},
+		{/*output frame for sw0 and sw1*/
+				.signals=				mSw0Sw1Signals,
+				.periodicity=			5,
+				.ID=					23,
+				.type=					LIN_FRAME_TYPE_SPORADIC,
+				.numberOfSignals=		2,
+				.frameSlotTimeTicks=	1,/*6.125 + jitter = 7ms*/
+				.frameSize=				4,
+				.direction=				LIN_FRAME_DIRECTION_PUBLISHER,
+				.firstDelay=			4
+		},
+		{/*input frame for sw2 and sw3*/
+				.signals=				mSw2Sw3Signals,
+				.periodicity=			5,
+				.ID=					40,
+				.type=					LIN_FRAME_TYPE_UNCONDITIONAL,
+				.numberOfSignals=		2,
+				.frameSlotTimeTicks=	1,/*6.854 + jitter = 8ms*/
+				.frameSize=				5,
+				.direction=				LIN_FRAME_DIRECTION_SUBSCRIBER,
+				.firstDelay=			4
+		}
+};
+
+static const LIN_frameConfigType cluster1Frames[4]=
+{
+		{/*input frame for led0 and led1 ID=4*/
+				.signals=				sL0L1Signals,
+				.periodicity=			10,
+				.ID=					4,
+				.type=					LIN_FRAME_TYPE_UNCONDITIONAL,
+				.numberOfSignals=		2,
+				.frameSlotTimeTicks=	1,/*3.9375 + jitter = 5ms*/
+				.frameSize=				1,
 				.direction=				LIN_FRAME_DIRECTION_SUBSCRIBER,
 				.firstDelay=			0
 		},
-		{
-				.signals=				frame_1_1_signals,
-				.periodicity=			0,
-				.ID=					3,
-				.type=					LIN_FRAME_TYPE_SPORADIC,
-				.numberOfSignals=		NUMBER_OF_FRAME_1_1_SIGNALS,
-				.frameSlotTimeTicks=	FRAME_1_1_TIME_SLOT,
-				.frameSize=				FRAME_1_1_SIZE,
+		{/*output frame for led2 and led3 ID=30*/
+				.signals=				sL2L3Signals,
+				.periodicity=			25,
+				.ID=					30,
+				.type=					LIN_FRAME_TYPE_UNCONDITIONAL,
+				.numberOfSignals=		2,
+				.frameSlotTimeTicks=	1,/*6.125 + jitter = 7ms*/
+				.frameSize=				4,
 				.direction=				LIN_FRAME_DIRECTION_PUBLISHER,
-				.firstDelay=			0
+				.firstDelay=			2
+		},
+		{/*input frame for sw0 and sw1*/
+				.signals=				sSw0Sw1Signals,
+				.periodicity=			5,
+				.ID=					23,
+				.type=					LIN_FRAME_TYPE_UNCONDITIONAL,
+				.numberOfSignals=		2,
+				.frameSlotTimeTicks=	1,/*6.125 + jitter = 7ms*/
+				.frameSize=				4,
+				.direction=				LIN_FRAME_DIRECTION_SUBSCRIBER,
+				.firstDelay=			4
+		},
+		{/*output frame for sw2 and sw3*/
+				.signals=				sSw2Sw3Signals,
+				.periodicity=			5,
+				.ID=					40,
+				.type=					LIN_FRAME_TYPE_EVENT_TRIG,
+				.numberOfSignals=		2,
+				.frameSlotTimeTicks=	1,/*6.854 + jitter = 8ms*/
+				.frameSize=				5,
+				.direction=				LIN_FRAME_DIRECTION_PUBLISHER,
+				.firstDelay=			4
 		}
 };
 
 const LIN_clusterConfigType clusters[NUMBER_OF_CLUSTERS]=
 {
-		{
+		{/*Master Node*/
 				.frames=			cluster0Frames,
 				.bitRate=			USART_BAUDRATE_19200,
 				.UART_ID=			USART_ID_USART1,
-				.UART_asyncMode=	USART_DMA_ASYNC_MODE,
+				.UART_asyncMode=	USART_INTERRUPT_ASYNC_MODE,
 				.masterTaskControl=	LIN_CLUSTER_MASTER_TASK_CONTROL_ENABLE,
-				.numberOfFrames=	NUMBER_OF_CLUSTER_0_FRAMES
+				.numberOfFrames=	4
 		},
-		{
+		{/*Slave Node*/
 				.frames=			cluster1Frames,
 				.bitRate=			USART_BAUDRATE_19200,
 				.UART_ID=			USART_ID_USART3,
 				.UART_asyncMode=	USART_INTERRUPT_ASYNC_MODE,
 				.masterTaskControl=	LIN_CLUSTER_MASTER_TASK_CONTROL_DISABLE,
-				.numberOfFrames=	NUMBER_OF_CLUSTER_1_FRAMES
+				.numberOfFrames=	4
 		}
 };
--- HAL/LIN/LIN_config.h
+++ HAL/LIN/LIN_config.h
@@ -1,50 +1,28 @@
 #ifndef LIN_CONFIG_H_
 #define LIN_CONFIG_H_
 
-/*
- * THeader_Nominal= 34 * TBit
- * TResponse_Nominal= 10 * (NData + 1) * TBit
- * TFrame_Nominal= THeader_Nominal + TResponse_Nominal
- * TFrame_Nominal= (34+10*(NData+1))*TBit
- * TFrame_Maximum=1.4*((34+10*(NData+1))*TBit)
- * TFrame_Maximum=1.4*((34+10*(NData+1))/BIT_RATE)
- * TFrame_Slot > jitter + TFrame_Maximum
- * */
+#define SIGNAL_LED0_M 0
+#define SIGNAL_LED1_M 1
+#define SIGNAL_LED0_S 2
+#define SIGNAL_LED1_S 3
+#define SIGNAL_LED2_M 4
+#define SIGNAL_LED3_M 5
+#define SIGNAL_LED2_S 6
+#define SIGNAL_LED3_S 7
 
-#define NUMBER_OF_CLUSTER_0_FRAMES 	2
-#define NUMBER_OF_FRAME_0_0_SIGNALS	2
-#define FRAME_0_0_SIZE				4
-#define FRAME_0_0_TIME_SLOT			2
-#define NUMBER_OF_FRAME_0_1_SIGNALS	2
-#define FRAME_0_1_SIZE				3
-#define FRAME_0_1_TIME_SLOT			2
-
-#define NUMBER_OF_CLUSTER_1_FRAMES 	2
-#define NUMBER_OF_FRAME_1_0_SIGNALS	2
-#define FRAME_1_0_SIZE				2
-#define FRAME_1_0_TIME_SLOT			2
-#define NUMBER_OF_FRAME_1_1_SIGNALS	2
-#define FRAME_1_1_SIZE				8
-#define	FRAME_1_1_TIME_SLOT			3
+#define SIGNAL_SW0_M 0
+#define SIGNAL_SW1_M 1
+#define SIGNAL_SW2_M 2
+#define SIGNAL_SW3_M 3
+#define SIGNAL_SW0_S 4
+#define SIGNAL_SW1_S 5
+#define SIGNAL_SW2_S 6
+#define SIGNAL_SW3_S 7
 
 #define NUMBER_OF_CLUSTERS 					2
-#define BASE_TIME							LIN_BASE_TIME_5ms
-#define NUMBER_OF_FRAMES					4
-#define NUMBER_OF_SCALER_SIGNALS			4
-#define NUMBER_OF_BYTE_ARRAY_SIGNALS		4
-
-#define SIGNAL0_ID	((u8)0)
-#define SIGNAL1_ID	((u8)1)
-#define SIGNAL2_ID	((u8)2)
-#define SIGNAL3_ID	((u8)3)
-#define SIGNAL4_ID	((u8)0)
-#define SIGNAL5_ID	((u8)1)
-#define SIGNAL6_ID	((u8)2)
-#define SIGNAL7_ID	((u8)3)
-
-#define SIGNAL4_LENGTH	((u8)3)
-#define SIGNAL5_LENGTH	((u8)2)
-#define SIGNAL6_LENGTH	((u8)1)
-#define	SIGNAL7_LENGTH	((u8)7)
+#define BASE_TIME							LIN_BASE_TIME_10ms
+#define NUMBER_OF_FRAMES					8
+#define NUMBER_OF_SCALER_SIGNALS			8
+#define NUMBER_OF_BYTE_ARRAY_SIGNALS		8
 
 #endif /* LIN_CONFIG_H_ */
