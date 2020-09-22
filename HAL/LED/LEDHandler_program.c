ec390bc28ee822cee71ba5722d3bfccb379177a1
YoussefKamal40 <youx199@gmail.com>
2020-09-22 14:50:07 +0200

    Update LEDHandler_program.c
    fixing HLED

--- HAL/LED/LEDHandler_program.c
+++ HAL/LED/LEDHandler_program.c
@@ -1,3 +1,8 @@
+/*
+ *
+ *  Created on: Sep 22, 2020
+ *      Author: Youssef Kamal
+ */
 #include "STD_Types.h"
 #include "RCC_interface.h"
 #include "LEDHandler_interface.h"
@@ -9,31 +14,31 @@ void LED_init(void)
 	u8 i;
 	for(i=0;i<NUMBER_OF_LEDS;i++)
 	{
-		GPIO_configPins(&ledsConfig[i].ledPin);
-		GPIO_setPinValue(ledsConfig[i].ledPin.port,ledsConfig[i].ledPin.pin,ledsConfig[i].initState^ledsConfig[i].activeState);
 		switch(ledsConfig[i].ledPin.port)
 		{
-		case GPIO_A_PORT:
+		case LEDH_A_PORT:
 			RCC_u8_ControlPrephiral(RCC_u32_CONTROL_PERIP_IOPAEN,RCC_u8_ENABLE);
 			break;
-		case GPIO_B_PORT:
+		case LEDH_B_PORT:
 			RCC_u8_ControlPrephiral(RCC_u32_CONTROL_PERIP_IOPBEN,RCC_u8_ENABLE);
 			break;
-		case GPIO_C_PORT:
+		case LEDH_C_PORT:
 			RCC_u8_ControlPrephiral(RCC_u32_CONTROL_PERIP_IOPCEN,RCC_u8_ENABLE);
 			break;
-		case GPIO_D_PORT:
+		case LEDH_D_PORT:
 			RCC_u8_ControlPrephiral(RCC_u32_CONTROL_PERIP_IOPDEN,RCC_u8_ENABLE);
 			break;
-		case GPIO_E_PORT:
+		case LEDH_E_PORT:
 			RCC_u8_ControlPrephiral(RCC_u32_CONTROL_PERIP_IOPEEN,RCC_u8_ENABLE);
 			break;
-		case GPIO_F_PORT:
+		case LEDH_F_PORT:
 			RCC_u8_ControlPrephiral(RCC_u32_CONTROL_PERIP_IOPFEN,RCC_u8_ENABLE);
 			break;
-		case GPIO_G_PORT:
+		case LEDH_G_PORT:
 			RCC_u8_ControlPrephiral(RCC_u32_CONTROL_PERIP_IOPGEN,RCC_u8_ENABLE);
 		}
+		GPIO_configPins(&ledsConfig[i].ledPin);
+		GPIO_setPinValue(ledsConfig[i].ledPin.port,ledsConfig[i].ledPin.pin,ledsConfig[i].initState^ledsConfig[i].activeState);
 	}
 }
 
